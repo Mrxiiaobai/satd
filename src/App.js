5cc6abf04bce8ebfee0d73d92f02c40ed1f6b07b
jiangming <jiangming@otosaas.com>
2020-08-03 14:14:11 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { Button } from './components';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        hello
-        <Button text={'qwer'}></Button>
+        1
       </div>
     );
   }
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './index.css';
 
-const Button = ({ text }) => <button className="btn">这是一个组件按钮{text}</button>
+export class Button extends Component{
+  constructor(props){
+    super(props)
+    this.state={
 
-Button.propTypes = {
-  text: PropTypes.any
-};
+    }
+  }
+
+  render(){
+    const {text} = this.props
+    return (
+      <button className="btn">这是一个组件按钮{text}</button>  
+    )
+  }
+}
+
+// Button.propTypes = {
+//   text: PropTypes.any
+// };
 
 export default Button;
